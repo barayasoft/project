26fc8ee47ea284d3819e2fc2fe336d875a4e5c9d
dadan <dadanhidyt@gmail.com>
2022-07-15 05:05:03 -0700

    dada

--- index.js
+++ index.js
@@ -1,13 +1,1 @@
-(function(){
-
-    function $el(el) {
-        return document.querySelector(el);
-    }
-
-    const button_start = $el("#start_button");
-    const stop_button = $el("#stop_button");
-    if(undefined == stop_bbutton) {
-        
-    }
-});
-
+console.log({});
