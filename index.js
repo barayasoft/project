533162c78e75575227b2dee49200f2ec82b154f1
dadan <dadanhidyt@gmail.com>
2022-07-15 04:45:46 -0700

    isi

---
+++ index.js
@@ -0,0 +1,9 @@
+(function(){
+
+    function $el(el) {
+        return document.querySelector(el);
+    }
+
+    const button_start = $el("#start_button");
+    const stop_button = $el("#stop_button");
+});
